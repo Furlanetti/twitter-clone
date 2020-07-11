30d05fc6c5b0e7b9da55371cb4b43c495fe386f3
furlanetti <felipe.furlanetti1@gmail.com>
2020-07-11 10:09:11 -0300

    work free time commit

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,1 +1,18 @@
 import React from 'react';
+
+import { Container, Wrapper } from './styles';
+import Main from '../Main';
+
+const Layout: React.FC = () => {
+    return (
+        <Container>
+            <Wrapper>
+                {/* <MenuBar /> */}
+                <Main />
+                {/* <SideBar /> */}
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Layout;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {Container,Header,BackIcon,ProfileInfo,BottomMenu,HomeIcon,SearchIcon,BellIcon,EmailIcon} from './styles';
+
+const Main: React.FC = () => {
+    return (
+        <Container>
+            <Header>
+                <button><BackIcon /></button>
+                <ProfileInfo>
+                    <strong>Felipe</strong>
+                    <span>12 tweets</span>
+                </ProfileInfo>
+            </Header>
+
+            {/* <ProfilePage /> */}
+
+            <BottomMenu>
+                <HomeIcon className="active"/>
+                <SearchIcon />
+                <BellIcon />
+                <EmailIcon />
+            </BottomMenu>
+
+        </Container>
+    );
+}
+
+export default Main;
